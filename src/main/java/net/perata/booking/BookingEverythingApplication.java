6744895b9fe4252f106f722b87d1b8e32b06841b
Roberto Perata <roberto.perata@milkmantechnologies.com>
2022-10-06 22:37:50 +0200

    init

---
+++ src/main/java/net/perata/booking/BookingEverythingApplication.java
@@ -0,0 +1,14 @@
+package net.perata.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingEverythingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BookingEverythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/perata/booking/BookingEverythingApplicationTests.java
@@ -0,0 +1,14 @@
+package net.perata.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingEverythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
